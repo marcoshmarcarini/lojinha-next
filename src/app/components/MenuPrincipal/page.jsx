5a4542713bf4081dceef0949306392974fbcf3a0
marcoshmarcarini <marcoshmarcarini@hotmail.com>
2023-08-23 21:57:35 -0300

    Update
    Update de dependências e adição do menu principal. Ainda estou definindo paleta de cores e logo..

---
+++ src/app/components/MenuPrincipal/page.jsx
@@ -0,0 +1,22 @@
+import styles from '@/app/components/MenuPrincipal/MenuPrincipal.module.css'
+
+export default function MenuPrincipal(){
+
+    return(
+        <>
+            <nav>
+                <div>
+                    NavBrand
+                </div>
+                <div>
+                    <ul>
+                        <li>Item 1</li>
+                        <li>Item 2</li>
+                        <li>Item 3</li>
+                        <li>Item 4</li>
+                    </ul>
+                </div>
+            </nav>
+        </>
+    )
+}
---
+++ src/app/page.jsx
@@ -0,0 +1,16 @@
+import MenuPrincipal from "./components/MenuPrincipal/page";
+
+
+export default function Home() {
+  return (
+    <>
+      <MenuPrincipal />
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <header>
+            
+        </header>
+      </main>
+    </>
+    
+  )
+}
